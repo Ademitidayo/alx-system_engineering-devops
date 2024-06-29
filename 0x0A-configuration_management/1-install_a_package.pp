447121d505d40336b4350a0fc5b08bce55924897
ademitidayo <adenijibimm08@gmail.com>
2024-06-28 19:43:17 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
 # install a package
 exec { 'install_flask':
-	command	=> '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
+	command	=> '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.1.1',
 	path	=> ['/usr/bin', '/usr/local/bin'],
 }
