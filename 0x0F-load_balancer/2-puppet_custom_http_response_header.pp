ab29753000a4a322d59bd819984497ee4c688a02
ademitidayo <adenijibimm08@gmail.com>
2024-07-08 17:46:44 -0700

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,67 +1,30 @@
 # Setup New Ubuntu server with nginx
 # and add a custom HTTP header
-# Ensure system is updated before proceeding
 
-exec { 'check update':
-  command => '/usr/bin/apt-get update -qq',
-  unless  => '/usr/bin/apt-get update -qq',
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['check update'],
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => @(END),
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    add_header X-Served-By <%= @hostname %>;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    if (\$request_filename ~ redirect_me) {
-        rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;
-    }
-
-    error_page 404 /error_404.html;
-    location = /error_404.html {
-        internal;
-    }
-}
-END
-  notify  => Service['nginx'],
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/default',
-  notify  => Service['nginx'],
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-exec { 'allow-nginx-http':
-  command => 'ufw allow "Nginx HTTP"',
-  unless  => 'ufw status | grep -q "Nginx HTTP"',
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-}
-
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  require    => Package['nginx'],
-  subscribe  => File['/etc/nginx/sites-available/default'],
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
