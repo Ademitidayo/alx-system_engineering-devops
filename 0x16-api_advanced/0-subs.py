0f91f63f909896334e389a4dda81918183ac5225
Ademitidayo <adenijibimmy08@gmail.com>
2024-08-19 22:27:31 +0100

    api advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """return number of total subscribers ona subreddit and 0 if invalid"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        retuurn 0
+    else
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
