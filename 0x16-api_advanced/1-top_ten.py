e472b50d2e868b96f52cfb6d6ab01a092a6be2d1
Ademitidayo <adenijibimmy08@gmail.com>
2024-08-30 00:39:34 +0100

    advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,17 +5,20 @@
 import requests
 from sys import argv
 
+
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    response = requests.get(url, headers=headers)
+    '''
+        returns top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-    if response.status_code != 200:
-        print("None")
-        return
 
-    data = response.json()
-    posts = data.get('data', {}).get('children', [])
-
-    for post in posts:
-        print(post.get('data', {}).get('title'))
+if __name__ == "__main__":
+    top_ten(argv[1])
